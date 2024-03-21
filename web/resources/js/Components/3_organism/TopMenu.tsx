20f1c891047580339b9d86308a819415e1c3a9ba
Alex Kampf <alex.kampf@key-tec.de>
2024-03-21 03:39:37 +0100

    menu large user menu

--- web/resources/js/Components/3_organism/TopMenu.tsx
+++ web/resources/js/Components/3_organism/TopMenu.tsx
@@ -13,10 +13,67 @@ export default function TopMenu() {
 
     const { props } = usePage();
     const categories = props.categories;
+    const user = props.auth?.user;
+
+    const userMenu = (
+        <div className="flex flex-row">
+            <Link
+                className="text-gray-500 hover:text-gray-700 flex flex-row" 
+                href={route('logout')} 
+                method="post"
+            >
+                <img 
+                    className="mr-2"
+                    src="/images/icons/logout.png" 
+                    alt="logo" 
+                    width="30" 
+                    height="30"
+                />
+                <div>
+                    Deconnexion
+                </div>
+            </Link>
+        </div>
+    )
+
+    const guestMenu = (
+        <div className="flex flex-row content-center ">
+            <Link
+                href={route('register')}
+                className="text-gray-500 hover:text-gray-700 flex flex-row pr-8"
+            >
+                <img 
+                    className="mr-2"
+                    src="/images/icons/new_user.png" 
+                    alt="logo" 
+                    width="30" 
+                    height="30"
+                />
+                <div>
+                    S'enregistrer
+                </div>
+            </Link>
+            <Link
+                href={route('login')}
+                className="text-gray-500 hover:text-gray-700 flex flex-row"
+            >
+                <img 
+                    className="mr-2"
+                    src="/images/icons/login.png" 
+                    alt="logo" 
+                    width="30" 
+                    height="30"
+                />
+                <div>
+                    Connexion
+                </div>
+            </Link>
+        </div>
+    )
 
     // large menu used for tablet and desktop
-    const menuLarge=(
-        <div className="flex flex-row justify-between h-[50px] border-b-2 px-40 w-full">
+    const menuLarge = (
+        <div className="flex flex-row justify-between h-[50px] border-b-2 px-40 w-full ">
             <div className="flex flex-row">
                 <Link href="/">
                     <img src="/images/logo.svg" alt="logo" width="50" height="50" />
@@ -67,12 +124,18 @@ export default function TopMenu() {
                     </MenuList>
                 </Menu>
             </div>
-            <div>right</div>
+            <div>
+                {user ? (
+                    <>{userMenu}</>
+                ) : (
+                    <>{guestMenu}</>
+                )}
+            </div>
         </div>
     )
 
     // narrow menu used by mobile phones
-    const menuNarrow=[
+    const menuNarrow = [
         <div className="lg:hidden flex flex-col w-full">
         </div>
     ]
