ab539364daf968c7ef168836c1eae47e9cb26358
Alex Kampf <alex.kampf@key-tec.de>
2024-04-08 00:45:30 +0200

    Favicon

--- web/resources/js/Layouts/Layout.tsx
+++ web/resources/js/Layouts/Layout.tsx
@@ -2,21 +2,29 @@
 import React from 'react';
 import TopMenu from '@/Components/3_cell/TopMenu';
 import BottomMenu from '@/Components/3_cell/BottomMenu';
+import { Head } from '@inertiajs/react';
 
 const Layout = ({ children }) => {
 
   return (
-    <div className="flex flex-col min-h-screen bg-slate-50">
-      <header>
-        <TopMenu />
-      </header>
-      <main className="container p-0 mx-auto">
-        {children}
-      </main>
-      <footer>
-        <BottomMenu />
-              </footer>
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
+      </Head>
+      
+      <div className="flex flex-col min-h-screen bg-slate-50">
+        <header>
+          <TopMenu />
+        </header>
+        <main className="container p-0 mx-auto">
+          {children}
+        </main>
+        <footer>
+          <BottomMenu />
+        </footer>
+      </div>
+    </>
+
     );
   };
   
