2c0aeaf12ceb6287614ceebc50e0d73d2f0d8283
Alex Kampf <alex.kampf@key-tec.de>
2024-04-20 00:12:00 +0200

    react-anchor-link-smooth-scroll

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,12 @@
+
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 
 /** @type {import('tailwindcss').Config} */
-export default {
+//export default {
+export default withMT({
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -21,9 +25,37 @@ export default {
                     400: '#A3D7E5',
                     500: '#69BBCF'
                 },
+                pink: {
+                    50: '#fdf2f8',
+                    100: '#fce7f3',
+                    200: '#fbcfe8',
+                    300: '#f9a8d4',
+                    400: '#f472b6',
+                    500: '#ec4899',
+                    600: '#db2777',
+                    700: '#be185d',
+                    800: '#9d174d',
+                    900: '#831843',
+                    950:'#500724'
+                },
+                sky:{
+
+                    50: '#f0f9ff',
+                    100: '#e0f2fe',
+                    200: '#bae6fd',
+                    300: '#7dd3fc',
+                    400: '#38bdf8',
+                    500: '#0ea5e9',
+                    600: '#0284c7',
+                    700: '#0369a1',
+                    800: '#075985',
+                    900: '#0c4a6e',
+                    950: '#082f49'
+                }
             },
         },
     },
 
     plugins: [forms],
-};
+//};
+});
