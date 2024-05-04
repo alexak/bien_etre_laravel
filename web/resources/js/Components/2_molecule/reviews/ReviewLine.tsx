a2cdf0c2a74cb052006ad651fe6cd3e7eedb6e96
Alex Kampf <alex.kampf@key-tec.de>
2024-05-04 02:57:32 +0200

    upvote review (WIP)

--- web/resources/js/Components/2_molecule/reviews/ReviewLine.tsx
+++ web/resources/js/Components/2_molecule/reviews/ReviewLine.tsx
@@ -1,7 +1,9 @@
 
-import { Card, CardBody } from "@material-tailwind/react";
+import { Button, Card, CardBody } from "@material-tailwind/react";
 import Rating from "@/Components/1_atom/Rating";
 import { DateTime } from "luxon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 export default function ReviewLine({review}){
 
@@ -13,11 +15,21 @@ export default function ReviewLine({review}){
         <Card className="mb-6 border">
             <CardBody className="flex flex-col">
                 <div className="flex flex-row justify-between">
-                    <Rating 
-                        readonly
-                        showNumbers={false}
-                        value={review.rating}
-                    />
+                    <div className="flex flex-row items-center h-[40px]">
+                        <Rating 
+                            readonly
+                            showNumbers={false}
+                            value={review.rating}
+                        />
+                        {/*
+                        <Button className="p-2 ml-10 text-pink-500 bg-white rounded-full">
+                            <FontAwesomeIcon 
+                                className="w-4 h-4 " 
+                                icon={faThumbsUp}
+                            />
+                        </Button>
+                        */}
+                    </div>
                     <div>
                         publié par <span className="font-bold capitalize">{review.user.name}</span> {getTimePeriod(review.created_at)}
                     </div>
