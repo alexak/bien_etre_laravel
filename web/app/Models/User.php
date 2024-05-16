2a1ee39d638855dd58859970a2f24cf3b5e01ad4
Alex Kampf <alex.kampf@key-tec.de>
2024-05-16 02:16:28 +0200

    french language

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -7,10 +7,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Mail\WelcomeEmail;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    private $name;
+    private $email;
+    private $email_verified_at;
+    private $email_token;
+    private $password;
+    //$rememberToken();
+
 
     /**
      * The attributes that are mass assignable.
@@ -62,5 +72,17 @@ class User extends Authenticatable
 
     public function reviews() {
         return $this->hasMany(Review::class, 'user_id');
+    }
+
+    public static function findByMail(string $email): ?User
+    {
+        return self::where('email', $email)->first();
+    }
+
+    public function sendEmailVerificationNotification() {
+        $mailData = [
+            'userMailToken' => $this->email_token
+        ];
+        Mail::to($this->email)->send(new WelcomeEmail($mailData));
     }
 }
